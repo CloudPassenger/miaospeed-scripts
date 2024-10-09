bd020aea61e67f9260f2ec5a922a4866a0e00e09
CloudPassenger <coliwatoshinzo@gmail.com>
2024-10-09 18:51:43 +0100

    ua: add curl

--- consts/ua.ts
+++ consts/ua.ts
@@ -2,8 +2,9 @@ const UA_WINDOWS =
   "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/129.0.0.0 Safari/537.36";
 const UA_ANDROID =
   "Mozilla/5.0 (Linux; Android 12; SM-G991U) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/101.0.4951.41 Mobile Safari/537.36";
+const UA_CURL = "curl/7.88.1";
 
 const SEC_CH_UA =
   'Google Chrome";v="129", "Not=A?Brand";v="8", "Chromium";v="129';
 
-export { UA_WINDOWS, UA_ANDROID, SEC_CH_UA };
+export { UA_WINDOWS, UA_ANDROID, UA_CURL, SEC_CH_UA };
